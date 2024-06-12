001f0eb682134e7fed4ffe0e5da8326594347017
hakeemee1 <91174671+hakeemee1@users.noreply.github.com>
2024-06-12 16:45:33 +0700

    use zod to validate form

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,76 +1,71 @@
 import { useForm } from "react-hook-form";
-import { FormData } from "@/types";
 import FormField from "./FormField";
-
+import { FormData, UserSchema } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
 function Form() {
   const {
     register,
     handleSubmit,
     formState: { errors },
     setError,
-  } = useForm<FormData>();
-  
+  } = useForm<FormData>({
+    resolver: zodResolver(UserSchema), // Apply the zodResolver
+  });
+
   const onSubmit = async (data: FormData) => {
-  	console.log("SUCCESS", data);
-  }
+    console.log("SUCCESS", data);
+  };
 
   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid col-auto">
-          <h1 className="text-3xl font-bold mb-4">
-            Zod & React-Hook-Form
-          </h1>
-          <FormField
-            type="email"
-            placeholder="Email"
-            name="email"
-            register={register}
-            error={errors.email}
-          />
-
-          <FormField
-            type="text"
-            placeholder="GitHub URL"
-            name="githubUrl"
-            register={register}
-            error={errors.githubUrl}
-          />
-
-          <FormField
-            type="number"
-            placeholder="Years of Experience (1 - 10)"
-            name="yearsOfExperience"
-            register={register}
-            error={errors.yearsOfExperience}
-            valueAsNumber
-          />
-
-          <FormField
-            type="password"
-            placeholder="Password"
-            name="password"
-            register={register}
-            error={errors.password}
-          />
-
-          <FormField
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            register={register}
-            error={errors.confirmPassword}
-          />
-          <FormField 
-          type="text"
-          placeholder="gender"
-          name="gender"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid col-auto">
+        <h1 className="text-3xl font-bold mb-4">Zod & React-Hook-Form</h1>
+        <FormField
+          type="email"
+          placeholder="Email"
+          name="email"
           register={register}
-          error={errors.gender} />
-          <button type="submit" className="submit-button">
-            Submit
-          </button>
-        </div>
-      </form>
+          error={errors.email}
+        />
+
+        <FormField
+          type="text"
+          placeholder="GitHub URL"
+          name="githubUrl"
+          register={register}
+          error={errors.githubUrl}
+        />
+
+        <FormField
+          type="number"
+          placeholder="Years of Experience (1 - 10)"
+          name="yearsOfExperience"
+          register={register}
+          error={errors.yearsOfExperience}
+          valueAsNumber
+        />
+
+        <FormField
+          type="password"
+          placeholder="Password"
+          name="password"
+          register={register}
+          error={errors.password}
+        />
+
+        <FormField
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          register={register}
+          error={errors.confirmPassword}
+        />
+
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </div>
+    </form>
   );
 }
 
--- types.ts
+++ types.ts
@@ -1,12 +1,11 @@
 import { FieldError, UseFormRegister } from "react-hook-form";
-
+import { z, ZodType } from "zod"; // Add new import
 export type FormData = {
     email: string;
     githubUrl: string;
     yearsOfExperience: number;
     password: string;
     confirmPassword: string;
-    gender: string;
   };
 
   export type FormFieldProps = {
@@ -27,3 +26,28 @@ export type FormData = {
   | "password"
   | "confirmPassword"
   | "gender";
+
+  export const UserSchema: ZodType<FormData> = z
+  .object({
+    email: z.string().email(),
+    githubUrl: z
+      .string()
+      .url()
+      .includes("github.com", { message: "Invalid GitHub URL" }),
+    yearsOfExperience: z
+      .number({
+        required_error: "required field",
+        invalid_type_error: "Years of Experience is required",
+      })
+      .min(1)
+      .max(10),
+    password: z
+      .string()
+      .min(8, { message: "Password is too short" })
+      .max(20, { message: "Password is too long" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // path of error
+  });
